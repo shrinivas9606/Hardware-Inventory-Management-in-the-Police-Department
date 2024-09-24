4882926c9b09283d06e2eebc4dcb6a84332648f8
shrinivas9606 <shrinivas.narhare.cs@ghrcem.raisoni.net>
2024-09-25 02:46:49 +0530

    final upadet server check

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,12 @@
 // server/config/db.js
 const mongoose = require('mongoose');
+require('dotenv').config();
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI)
+        .then(() => console.log("MongoDB connected"))
+        .catch(err => console.error("MongoDB connection error:", err));
+    
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error(err.message);
